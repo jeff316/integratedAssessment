64920f1d8195e357150785a5cc305a0c030e78d6
Jefferson Lima <oliveiralima.jefferson@gmail.com>
2021-12-01 00:25:55 +0000

    First Push
    Inserted method to establish connection between Java and database mysql

---
+++ src/dal/ConnectionModule.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dal;
+
+import java.sql.*;
+
+
+/**
+ *
+ * @author Jefferson
+ */
+public class ConnectionModule {
+    //connects to database
+    public static Connection conector(){
+        Connection con = null;
+    //call the driver
+        String driver ="com.mysql.cj.jdbc.Driver";
+    //store information about the database
+        String url = "jdbc:mysql://localhost:3306/dbequationsolver";
+        String user = "root";
+        String password = "root";
+     //establishing connection with the database
+        try {
+            Class.forName(driver);
+            con = DriverManager.getConnection(url ,user, password);
+            return con;
+            
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+            
+        }
+    }     
+}
